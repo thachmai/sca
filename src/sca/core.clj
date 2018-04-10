b6a271b785c3565cf6cbafaae821598be67adc87
Thach Mai <contact@thachmai.info>
2018-04-10 06:43:20 -0700

    Added hiccup template to generate a summary index.html

--- src/sca/core.clj
+++ src/sca/core.clj
@@ -41,8 +41,10 @@
         js-parser (fn [node]
                     (->> node
                          .toString
-                         (re-find #"[^\"]*resolution\=4k")))]
-    (println "Processing " catalog-episode)
+                         (re-find #"[^\"]*resolution\=4k")))
+        video-url (str "videos/"
+                       (second (re-find #"/catalog/(.*)" (:sub-url catalog-episode)))
+                       ".mp4")]
     (Thread/sleep 20000) ; sleep 20s to avoid DoS the server
     (->
      (extract parsed
@@ -51,23 +53,48 @@
               "video" (attr :id)
               ; video source presents a little fun challenge for us
               ; since the source is actually dynamically injected by javascript, we parse the js!
-              ".catalog_show_page > .row > script" js-parser
-              )
+              ".catalog_show_page > .row > script" js-parser)
+     (merge {:video-url video-url})
      ((fn [details]
         (file-download
          (url (:video-source details))
-         (str directory "videos/" (:video-id details) ".mp4"))
+         (str directory (:video-url details)))
         details))
      (merge catalog-episode))))
-; (catalog-page {:sub-url "/screencasts/catalog/pretty-git-logs" :id "test-id"})
+; (episode-page {:sub-url "/screencasts/catalog/pretty-git-logs" :id "test-id"})
 
-#_(def full (slurp (str directory "destroy-all-episodes.edn")))
-#_(defn- generate-hiccup [catalog]
-  (let [season-mapper #([:h1 ])])
-  (html
-   [:html
-    (map #() catalog)
-    ]))
+#_(generate-hiccup (clojure.edn/read-string (slurp (str directory "destroy-all-episodes.edn"))))
+(defn- generate-hiccup [catalog]
+  (let [css ".on { display: block; } .off { display: none; }"
+        button-js "document.querySelector('div.on') && (document.querySelector('div.on').className='off');
+                   this.parentElement.parentElement.nextSibling.className='on';
+                   document.querySelector('.button-on') && (document.querySelector('.button-on').className='button button-outline');
+                   this.className='button button-on';"
+        episode-mapper #(identity [:section
+                                   [:div.row
+                                    [:div.column.column-33 {:style "font-weight: bold;"} (:title %1)]
+                                    [:div.column.column-57 (:sub-title %1)]
+                                    [:div.column.column-10 {:style "text-align: right;"}
+                                     [:a.button.button-outline {:onclick button-js} "View"]]]
+                                   [:div.off
+                                    [:div.row
+                                     [:div.column (:description-html %1)]]
+                                    [:div.row
+                                     [:div.column
+                                      [:video {:src (:video-url %1) :width "1080" :controls "yes"}]]]]])
+        season-mapper #(identity [:div.container {:style "margin-bottom: 3em;"}
+                                  [:h1(:header %1)]
+                                  (map episode-mapper (:episodes %1))])]
+    (->>
+     (html
+      [:html
+       [:link {:rel "stylesheet" :href "//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"}]
+       [:link {:rel "stylesheet" :href "//cdn.rawgit.com/necolas/normalize.css/master/normalize.css"}]
+       [:link {:rel "stylesheet" :href "//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css"}]
+       [:style css]
+       [:body {:style "margin-top: 6em"}
+        (map season-mapper catalog)]])
+     (spit (str directory "index.html")))))
 
 (defn -main
   "Execution entry point"
@@ -79,4 +106,4 @@
                         (merge catalog {:episodes episodes})))
         full (map top-to-full top)]
     (clojure.pprint/pprint full (clojure.java.io/writer (str directory "destroy-all-episodes.edn")))
-    ))
+    (generate-hiccup full)))
